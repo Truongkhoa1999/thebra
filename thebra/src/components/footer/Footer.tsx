ffd8fdd722cb68402656701fc78289570044d73c
Truongkhoa1999 <tdkhoa.aqua@gmail.com>
2023-06-13 13:07:35 +0300

    ditme

--- thebra/src/components/footer/Footer.tsx
+++ thebra/src/components/footer/Footer.tsx
@@ -4,16 +4,40 @@ import "./style/Footer.scss";
 export const Footer = () => {
   return (
     <div className="footer_container">
-      <div className="language_container">
-        <p>En &#9662;</p>
-      </div>
-      <div className="sitemap_container">
-        <p>Contact/</p>
-        <p> About</p>
-      </div>
-      <div className="media_container">
-        <p>Fb/</p>
-        <p> Ig</p>
+      <h1>TheBra</h1>
+      <div className="lower">
+        <div className="customerservice_container">
+          <h1>Customer service</h1>
+          <p>
+            HAIR & BEAUTY CARE are our passions at Lyko, as is providing
+            absolutely first-class CUSTOMER SERVICE. We'll help you figure out
+            how to make your curls last longer, which lipstick goes with your
+            weekend outfit, which beard trimmer gives the best results or where
+            in the world the order confirmation disappeared - we're here for
+            you! You can find our contact information and customer service
+            opening hours below. There are no stupid questions - feel free to
+            contact us! Psst! You can find answers to the most common questions
+            on the Frequently Asked Questions page.
+          </p>
+          <p>
+            VISITING HOURS: Chat Mon-Fri 09:00 - 16:00 By e-mail:
+            kaskapalvelu@lyko.fi
+          </p>
+        </div>
+        <div className="information_container">
+          <h1>Customer service</h1>
+          <p>
+            
+            Club Lyko My Pages Club Lyko Privacy Policy FAQ - Frequently Asked
+            Questions Privacy Notice Cookie Policy Order Terms About Lyko
+            Recommend to a friend Are you interested in cooperation with Lyko?
+            Toplist Discount codes Michael Edwards Fragrances of the World
+          </p>
+        </div>
+        <div className="newsletter_container">
+          <p>Fb/</p>
+          <p> Ig</p>
+        </div>
       </div>
     </div>
   );
