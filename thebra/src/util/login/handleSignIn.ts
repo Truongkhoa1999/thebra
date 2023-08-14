cde7f70eadc492c3450d47c2d4c6d00361dd0dbf
Khoa <tdkhoa.aqua@gmail.com>
2023-08-14 13:44:44 +0300

    rework api

--- thebra/src/util/login/handleSignIn.ts
+++ thebra/src/util/login/handleSignIn.ts
@@ -3,7 +3,7 @@ import { setToken, setWarning } from '../../redux/actions/getToken';
 export const handleSignIn = (username: string, password: string, navigate: Function) => {
   return async (dispatch: any) => {
     try {
-      const response = await fetch('https://thelingerebe.onrender.com/api/v1/customers/signin', {
+      const response = await fetch('https://thebrabe.onrender.com/api/v1/customers/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
