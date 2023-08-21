c5770bfe13c81952a28222e0e1c85926be0cf7e2
Khoa <tdkhoa.aqua@gmail.com>
2023-08-21 19:06:36 +0300

    2ready

--- thebra/src/components/products/RelevantProductsByCategory.tsx
+++ thebra/src/components/products/RelevantProductsByCategory.tsx
@@ -3,39 +3,49 @@ import { ProductProps } from "../../type/ProductProps";
 import { useEffect, useState } from "react";
 import { detectRelevantItemsByCategory } from "../../util/productByCategory/filterProductByCategory";
 import { AppDispatch, RootState } from "../../redux/store";
-import './style/RelevantProductById.scss'
+import "./style/RelevantProductById.scss";
 import { fetchProducts } from "../../redux/actions/getProducts";
+import { Link } from "react-router-dom";
 
-export const RelevantProductsByCategory = ({ category }: { category: string }) => {
-  const { products } = useSelector((state: RootState) => state.products)
-  const [relevantProductsByCategory, setRelevantProductsByCategory] = useState<ProductProps[]>(products)
-  const dispatch = useDispatch<AppDispatch>()
+export const RelevantProductsByCategory = ({
+  category,
+}: {
+  category: string;
+}) => {
+  const { products } = useSelector((state: RootState) => state.products);
+  const [relevantProductsByCategory, setRelevantProductsByCategory] =
+    useState<ProductProps[]>(products);
+  const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    dispatch(fetchProducts())
+    dispatch(fetchProducts());
   }, [dispatch]);
 
   useEffect(() => {
     if (products.length > 0) {
-      const relevantProducts = detectRelevantItemsByCategory(category, products)
-      setRelevantProductsByCategory(relevantProducts)
+      const relevantProducts = detectRelevantItemsByCategory(
+        category,
+        products
+      );
+      setRelevantProductsByCategory(relevantProducts);
     }
   }, [category, products]);
   return (
     <div className="relevant_container">
       <h2 className="heading">{category}</h2>
       <div className="relevant-items">
-        {
-          relevantProductsByCategory?.map(p => (
-            <div className="item">
-              <img className="item-img" src={p.images[2]} />
-              <div className="item-text">
-                <h2>{p.title}</h2>
-                <h2>{p.price}</h2></div>
+        {relevantProductsByCategory?.map((p) => (
+          <Link className="item" to={`/product/${p.id}`} key={p.id}>
+            {/* <div className="item"> */}
+            <img className="item-img" src={p.images[2]} />
+            <div className="item-text">
+              <h2>{p.title}</h2>
+              <h2>{p.price}</h2>
             </div>
-          ))
-        }
+            {/* </div> */}
+          </Link>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
