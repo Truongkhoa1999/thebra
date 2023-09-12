34c3721e6c928249abe9138f9f810c1c34019f99
Khoa <tdkhoa.aqua@gmail.com>
2023-09-12 22:27:11 +0300

    18Siu

--- thebra/src/components/cartreport/CartHeadingForDeliveryMethod.tsx
+++ thebra/src/components/cartreport/CartHeadingForDeliveryMethod.tsx
@@ -4,7 +4,7 @@ export const CartHeadingForDeliveryMethod = () => {
         <div className='heading'>
             <h3>Your Delivery Method</h3>
             <h4>Free shipping within Finland for orders over 49€.</h4>
-            <h4>The delivery fee changes due to selected country    .</h4>
+            <h4>The delivery fee changes due to selected country.</h4>
 
         </div>
     )
